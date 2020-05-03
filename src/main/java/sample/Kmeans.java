0d1ef6e08ba3b214827944b1ff04df8a46cb2581
Ouss <karakoradoffy@gmail.com>
2020-05-03 17:30:35 +0100

    claustering convergence

--- src/main/java/sample/Kmeans.java
+++ src/main/java/sample/Kmeans.java
@@ -34,29 +34,49 @@ public class Kmeans {
     }
 
 
-    public ArrayList<Cluster> clauster(){
-        ArrayList<Coordonée> points=new ArrayList<>(this.input.cammon());
-        ArrayList<Cluster> clausters=new ArrayList<>(initialClausters(this.k,points));
-        ArrayList<Coordonée> centers=new ArrayList<>();
-        for (int i = 0; i <clausters.size() ; i++) {
+    public ArrayList<Cluster> clauster() {
+        ArrayList<Coordonée> points = new ArrayList<>(this.input.cammon());
+        ArrayList<Cluster> clausters = new ArrayList<>(initialClausters(this.k, points));
+        ArrayList<Coordonée> centers = new ArrayList<>();
+        for (int i = 0; i < clausters.size(); i++) {
             centers.add(clausters.get(i).getCoordonées().get(0));
         }
-       double min=Double.MAX_VALUE;
-        int index=0;
-        for(Coordonée point:points){
-            for (int i = 0; i <centers.size() ; i++) {
-                if(min>point.distance(centers.get(i))){
-                    min=point.distance(centers.get(i));
-                    index=i;
+
+        double version=1;
+        ArrayList<Double> versions=new ArrayList<>();
+       while (true) {
+            double min = Double.MAX_VALUE;
+            int index = 0;
+            for (Coordonée point : points) {
+                for (int i = 0; i < centers.size(); i++) {
+                    if (min > point.distance(centers.get(i))) {
+                        min = point.distance(centers.get(i));
+                        index = i;
+                    }
                 }
+                clausters.get(index).getCoordonées().add(point);
+                centers.set(index, point.center(centers.get(index)));
+                min = Double.MAX_VALUE;
+                index = 0;
             }
-            clausters.get(index).getCoordonées().add(point);
-            centers.set(index,point.center(centers.get(index)));
-            min=Double.MAX_VALUE;
-            index=0;
+            for (int i = 0; i <centers.size() ; i++) {
+                version=version*centers.get(i).getX();
+            }
+           versions.add(version);
+
+           if (versions.size()>1 && versions.get(versions.size() - 1).equals(versions.get(versions.size() - 2)))
+                break;
+           else
+            {
+                for (int i = 0; i <clausters.size() ; i++) {
+                    clausters.get(i).getCoordonées().clear();
+                }
+           }
+
         }
 
-         return clausters;
+        return clausters;
+
     }
 
 
@@ -64,7 +84,7 @@ public class Kmeans {
         ArrayList<Cluster> clausters=new ArrayList<>();
 
         for (int i = 0; i <k ; i++) {
-            int randomIndex=1+(int)(Math.random()*points.size());
+            int randomIndex=1+(int)(Math.random()*points.size()-1);
             clausters.add(new Cluster("#12"+i+"78"+i,new ArrayList<>()));
             clausters.get(i).getCoordonées().add(points.get(randomIndex));
         }
