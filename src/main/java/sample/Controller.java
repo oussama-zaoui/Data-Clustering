75911672946d18668440964e463c00b024f6048f
Ouss <karakoradoffy@gmail.com>
2020-05-13 03:39:56 +0100

    split plot and cluster methode

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -36,14 +36,20 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
 
+
+
+    }
+
+
+    public void plot(ActionEvent evente){
         ImportData data=new ImportData();
         try{
-         dataSet=data.readExcel("/home/ouss/maDATA.xlsx");
+            dataSet=data.readExcel("/home/ouss/maDATA.xlsx");
 
             for (int i = 0; i <dataSet.getRow() ; i++) {
                 for (int j = 0; j <dataSet.getCol() ; j++) {
                     System.out.print(dataSet.getData()[i][j]);
-                                    }
+                }
                 System.out.println();
             }
 
@@ -51,7 +57,7 @@ public class Controller implements Initializable {
             acp.centerAndReduce();
             ArrayList<Coordonée> coordonées=new ArrayList<>(acp.cammon());
 
-           NumberAxis  XAxis=new NumberAxis(-5,5,0.5);
+            NumberAxis  XAxis=new NumberAxis(-5,5,0.5);
             NumberAxis YAxis=new NumberAxis(-5,5,0.5);
 
             ScatterChart chart=new ScatterChart<>(XAxis,YAxis);
@@ -70,21 +76,21 @@ public class Controller implements Initializable {
         }catch (IOException e){
             e.printStackTrace();
         }
-
     }
 
 
-    public void plot(ActionEvent evente){
-            pane.getChildren().clear();
-            kmeans=new Kmeans(acp,Integer.valueOf(k.getText()));
-            ArrayList<Cluster> clusters=new ArrayList<>(kmeans.clauster(Integer.valueOf(maxIterations.getText())));
+    public void clusterize(ActionEvent event){
+
+        pane.getChildren().clear();
+        kmeans=new Kmeans(acp,Integer.parseInt(k.getText()));
+        ArrayList<Cluster> clusters=new ArrayList<>(kmeans.clauster(Integer.parseInt(maxIterations.getText())));
         final NumberAxis xAxis=new NumberAxis(-5,5,0.5);
         final NumberAxis yAxis=new NumberAxis(-5,5,0.5);
         ScatterChart<Number,Number> chart= new ScatterChart<>(xAxis, yAxis);
 
         for (int i = 0; i <clusters.size() ; i++) {
 
-                XYChart.Series series1=new XYChart.Series();
+            XYChart.Series series1=new XYChart.Series();
 
             for (int j = 0; j <clusters.get(i).getCoordonées().size() ; j++) {
                 series1.getData().add(new XYChart.Data(clusters.get(i).getCoordonées().get(j).getX(),clusters.get(i).getCoordonées().get(j).getY()));
